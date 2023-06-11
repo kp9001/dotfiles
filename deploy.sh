c097f89059c2ff9cca99e62c9431af3fb9b7442e
Kenneth Plante <kplante@kplante.com>
2023-06-11 14:57:10 -0400

    simple deployment script for servers

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir ~/.vim
+curl https://raw.githubusercontent.com/kp9001/dotfiles/master/vim/vimrc -o ~/.vim/vimrc
+
+touch ~/.mac_tmux.conf
+curl https://raw.githubusercontent.com/kp9001/dotfiles/master/tmux/theme_tmux.conf -o ~/.theme_tmux.conf
+curl https://raw.githubusercontent.com/kp9001/dotfiles/master/tmux/tmux.conf -o ~/.tmux.conf
+
+mv ~/.bashrc ~/.bashrc.old
+curl https://raw.githubusercontent.com/kp9001/dotfiles/master/shell/bashrc -o ~/.bashrc
+source ~/.bashrc
