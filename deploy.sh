b3457362dc9ef98ac778ab227e92934a158059f4
Kenneth Plante <kplante@kplante.com>
2023-06-11 15:24:03 -0400

    Update deploy.sh
    can't source like this

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,3 @@ curl https://raw.githubusercontent.com/kp9001/dotfiles/master/tmux/tmux.conf -o 
 
 mv ~/.bashrc ~/.bashrc.old
 curl https://raw.githubusercontent.com/kp9001/dotfiles/master/shell/bashrc -o ~/.bashrc
-source ~/.bashrc
